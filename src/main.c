b6adb4c80875b3b2145e72b8b7c1b05c0d7b112d
{brackets} <127058566+semibrackets@users.noreply.github.com>
2025-12-02 13:42:44 -0600

    WIP Update main.c

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@
    feature
 
    add bigger map, keep window size same, just move the stuff to the left:
-    when the player tries to move past the border, increase width:
+    when the player tries to move past the border, increase width or something like that
 
    add saving and loading
 
@@ -22,11 +22,15 @@
    add line of sight, player can only see so far. If the player is in a room the player can completly see the room unless the room is really big or something 
 */
 
-#define WIDTH 60
-#define LENGTH 30
-#define HEIGHT 10
+#define gameWIDTH 60
+#define gameLENGTH 30
+#define gameHEIGHT 10
+
+#define inventoryWIDTH 60
+#define inventoryLENGTH 10
 
 bool running = true;
+// bool airborn = false;
 
 typedef struct 
 {
@@ -35,19 +39,19 @@ typedef struct
 } Entity;
 
 //          z        y      x
-char room[HEIGHT][LENGTH][WIDTH];
-const char blocks[] = {' ', '.', '%', '#', '|', '-'};
+char room[gameHEIGHT][gameLENGTH][gameWIDTH];
+const char blocks[] = {' ', ';', '%', '#', '|', '-'};
 int selectedBlock = 0;
 
 void generate(void) 
 {
-  for (int z = 0; z < HEIGHT; z++) 
+  for (int z = 0; z < gameHEIGHT; z++) 
   {
-    for (int y = 0; y < LENGTH; y++) 
+    for (int y = 0; y < gameLENGTH; y++) 
     {
-      for (int x = 0; x < WIDTH; x++) 
+      for (int x = 0; x < gameWIDTH; x++) 
       {
-        if (y == 0 || x == 0 || y == LENGTH - 1 || x == WIDTH - 1) 
+        if (y == 0 || x == 0 || y == gameLENGTH - 1 || x == gameWIDTH - 1) 
         {
           room[z][y][x] = blocks[0]; // air
         } 
@@ -58,7 +62,9 @@ void generate(void)
         else if (z > 0 && z < 4)   // z 1, z 2, z 3
         {
           room[z][y][x] = blocks[2]; // dirt
-        } else if (z >= 4) {
+        } 
+        else if (z >= 4) 
+        {
           room[z][y][x] = blocks[3]; // stone
         }
       }
@@ -66,47 +72,54 @@ void generate(void)
   }
 }
 
-void render(WINDOW *win, Entity *player) 
+void render(WINDOW *game, WINDOW *inventory, Entity *player) 
 {
-  wclear(win);
-  box(win, 0, 0);
-  mvwprintw(win, 0, 1, "| %d/%d | Selected block: [ %c ] | Dev: semibrackets |", player->z, HEIGHT, blocks[selectedBlock]);
-  mvwprintw(win, LENGTH - 1, 1, "| the TERMICRAFT project | ver: 0.0.0-alpha-dev |");
+  wclear(game);
+  wclear(inventory);
+  box(game, 0, 0);
+  box(inventory, 0, 0); 
+  mvwprintw(game, 0, 1, "| %d/%d | Selected block: [ %c ] |", player->z, gameHEIGHT, blocks[selectedBlock]); // game
+  mvwprintw(game, gameLENGTH - 1, 1, "| the TERMICRAFT project | ver: 0.0.0-alpha-dev |"); // game
+  
+  mvwprintw(inventory, 0, 1, "| inv |"); // inv
 
-  for (int y = 1; y < LENGTH - 1; y++) 
+  for (int y = 1; y < gameLENGTH - 1; y++) 
   {
-    for (int x = 1; x < WIDTH - 1; x++) 
+    for (int x = 1; x < gameWIDTH - 1; x++) 
     {
-      if (y == player->y && x == player->x && player->z >= 0 && player->z < HEIGHT) 
+      if (y == player->y && x == player->x && player->z >= 0 && player->z < gameHEIGHT) 
       {
-        mvwaddch(win, y, x, player->icon);
+        mvwaddch(game, y, x, player->icon);
       } 
       else 
       {
-        switch (room[player->z][y][x]) 
+        if (room[player->z][y][x] == blocks[1])
         {
-        case '.':
-          wattron(win, COLOR_PAIR(1));
-          mvwaddch(win, y, x, room[player->z][y][x]);
-          wattroff(win, COLOR_PAIR(1));
-          break;
-        case '%':
-          wattron(win, COLOR_PAIR(2));
-          mvwaddch(win, y, x, room[player->z][y][x]);
-          wattroff(win, COLOR_PAIR(2));
-          break;
-        case '#':
-          wattron(win, COLOR_PAIR(3));
-          mvwaddch(win, y, x, room[player->z][y][x]);
-          wattroff(win, COLOR_PAIR(3));
-          break;
-        default:
-          mvwaddch(win, y, x, room[player->z][y][x]);
-          break;
+          wattron(game, COLOR_PAIR(1));
+          mvwaddch(game, y, x, room[player->z][y][x]);
+          wattroff(game, COLOR_PAIR(1));
+        }
+        else if (room[player->z][y][x] == blocks[2])
+        {
+          wattron(game, COLOR_PAIR(2));
+          mvwaddch(game, y, x, room[player->z][y][x]);
+          wattroff(game, COLOR_PAIR(2));
+        }
+        else if (room[player->z][y][x] == blocks[3])
+        {
+          wattron(game, COLOR_PAIR(3));
+          mvwaddch(game, y, x, room[player->z][y][x]);
+          wattroff(game, COLOR_PAIR(3));
+        }
+        else
+        {
+          mvwaddch(game, y, x, room[player->z][y][x]);
         }
       }
     }
   }
+
+  mvwprintw(inventory, 2, 1, "[ %c ]", blocks[selectedBlock]);
 }
 
 void mineBlock(Entity *player) 
@@ -123,11 +136,11 @@ void mineBlock(Entity *player)
       room[player->z][player->y][player->x - 1] = blocks[0];
     break;
   case 's': // mine south
-    if (player->y < LENGTH - 2)
+    if (player->y < gameLENGTH - 2)
       room[player->z][player->y + 1][player->x] = blocks[0];
     break;
   case 'd': // mine west
-    if (player->x < WIDTH - 2)
+    if (player->x < gameWIDTH - 2)
       room[player->z][player->y][player->x + 1] = blocks[0];
     break;
   case 'q': // mine northeast
@@ -135,19 +148,19 @@ void mineBlock(Entity *player)
       room[player->z][player->y - 1][player->x - 1] = blocks[0];
     break;
   case 'e': // mine northwest
-    if (player->y > 1 && player->x < WIDTH - 2)
+    if (player->y > 1 && player->x < gameWIDTH - 2)
       room[player->z][player->y - 1][player->x + 1] = blocks[0];
     break;
   case 'z': // mine southeast
-    if (player->y < LENGTH - 2 && player->x > 1)
+    if (player->y < gameLENGTH - 2 && player->x > 1)
       room[player->z][player->y + 1][player->x - 1] = blocks[0];
     break;
   case 'c': // mine southwest
-    if (player->y < LENGTH - 2 && player->x < WIDTH - 2)
+    if (player->y < gameLENGTH - 2 && player->x < gameWIDTH - 2)
       room[player->z][player->y + 1][player->x + 1] = blocks[0];
     break;
-  case KEY_RIGHT: // mine at player pos
-    if (player->z < HEIGHT - 1)
+  case KEY_LEFT: // mine at player pos
+    if (player->z < gameHEIGHT - 1)
       room[player->z][player->y][player->x] = blocks[0];
     break;
   case 'W': // mine above NOTE: temporary (probably)
@@ -155,7 +168,7 @@ void mineBlock(Entity *player)
       room[player->z - 1][player->y][player->x] = blocks[0];
     break;
   case 'S': // mine below NOTE: temporary (probably)
-    if (player->z < HEIGHT - 1)
+    if (player->z < gameHEIGHT - 1)
       room[player->z + 1][player->y][player->x] = blocks[0];
     break;
   default:
@@ -175,43 +188,31 @@ void placeBlock(Entity *player)
     if (player->x > 1) room[player->z][player->y][player->x - 1] = blocks[selectedBlock];
     break;
   case 's': // build south
-    if (player->y < LENGTH - 2) room[player->z][player->y + 1][player->x] = blocks[selectedBlock];
+    if (player->y < gameLENGTH - 2) room[player->z][player->y + 1][player->x] = blocks[selectedBlock];
     break;
   case 'd': // build east
-    if (player->x < WIDTH - 2) room[player->z][player->y][player->x + 1] = blocks[selectedBlock];
+    if (player->x < gameWIDTH - 2) room[player->z][player->y][player->x + 1] = blocks[selectedBlock];
     break;
   case 'q': // build northeast
-    if (player->y > 1) 
-    {
-      if (player->x > 1) room[player->z][player->y - 1][player->x - 1] = blocks[selectedBlock];
-    }
+    if (player->y > 1 && player->x > 1) room[player->z][player->y - 1][player->x - 1] = blocks[selectedBlock];
     break;
   case 'e': // build northwest
-    if (player->y > 1) 
-    {
-      if (player->x < WIDTH - 2) room[player->z][player->y - 1][player->x + 1] = blocks[selectedBlock];
-    }
+    if (player->y > 1 && player->x < gameWIDTH - 2) room[player->z][player->y - 1][player->x + 1] = blocks[selectedBlock];
     break;
   case 'z': // build southeast
-    if (player->y < LENGTH - 2) 
-    {
-      if (player->x > 1) room[player->z][player->y + 1][player->x - 1] = blocks[selectedBlock];
-    }
+    if (player->y < gameLENGTH - 2 && player->x > 1) room[player->z][player->y + 1][player->x - 1] = blocks[selectedBlock];
     break;
   case 'c': // build southwest
-    if (player->y < LENGTH - 2) 
-    {
-      if (player->x < WIDTH - 2) room[player->z][player->y + 1][player->x + 1] = blocks[selectedBlock];
-    }
+    if (player->y < gameLENGTH - 2 && player->x < gameWIDTH - 2) room[player->z][player->y + 1][player->x + 1] = blocks[selectedBlock];
     break;
   case KEY_RIGHT: // build at players pos
-    if (player->z < HEIGHT - 1) room[player->z][player->y][player->x] = blocks[selectedBlock];
+    if (player->z < gameHEIGHT - 1) room[player->z][player->y][player->x] = blocks[selectedBlock];
     break;
   case 'W': // build above
     if (player->z > 0) room[player->z - 1][player->y][player->x] = blocks[selectedBlock];
     break;
   case 'S':
-    if (player->z < HEIGHT - 1) room[player->z + 1][player->y][player->x] = blocks[selectedBlock];
+    if (player->z < gameHEIGHT - 1) room[player->z + 1][player->y][player->x] = blocks[selectedBlock];
     break;
   default:
     break;
@@ -235,13 +236,13 @@ void movePlayer(Entity *player, char direction)
       player->x--; // move left
     break;
   case 's': // move south
-    if (player->y + 1 != LENGTH - 1 &&
+    if (player->y + 1 != gameLENGTH - 1 &&
         room[player->z][player->y + 1][player->x] != '#' &&
         room[player->z][player->y + 1][player->x] != '%')
       player->y++; // move down
     break;
   case 'd': // move west
-    if (player->x + 1 != WIDTH - 1 &&
+    if (player->x + 1 != gameWIDTH - 1 &&
         room[player->z][player->y][player->x + 1] != '#' &&
         room[player->z][player->y][player->x + 1] != '%')
       player->x++; // move right
@@ -255,7 +256,7 @@ void movePlayer(Entity *player, char direction)
     }
     break;
   case 'e': // move north west
-    if (player->y - 1 != 0 && player->x + 1 != WIDTH - 1 &&
+    if (player->y - 1 != 0 && player->x + 1 != gameWIDTH - 1 &&
         room[player->z][player->y - 1][player->x + 1] != '#' &&
         room[player->z][player->y - 1][player->x + 1] != '%') {
       player->y--;
@@ -263,7 +264,7 @@ void movePlayer(Entity *player, char direction)
     }
     break;
   case 'z': // move south east
-    if (player->y + 1 != LENGTH - 1 && player->x - 1 != 0 &&
+    if (player->y + 1 != gameLENGTH - 1 && player->x - 1 != 0 &&
         room[player->z][player->y + 1][player->x - 1] != '#' &&
         room[player->z][player->y + 1][player->x - 1] != '%') {
       player->y++;
@@ -271,7 +272,7 @@ void movePlayer(Entity *player, char direction)
     }
     break;
   case 'c': // move south west
-    if (player->y < LENGTH - 2 && player->x < WIDTH - 2 &&
+    if (player->y < gameLENGTH - 2 && player->x < gameWIDTH - 2 &&
         room[player->z][player->y + 1][player->x] != '#' &&
         room[player->z][player->y + 1][player->x] != '%') {
       player->y++;
@@ -283,7 +284,7 @@ void movePlayer(Entity *player, char direction)
       player->z--;
     break;
   case 'S':
-    if (player->z < HEIGHT - 1)
+    if (player->z < gameHEIGHT - 1)
       player->z++;
     break;
   default:
@@ -302,6 +303,10 @@ void getInput(Entity *player)
     running = false;
     return;
   }
+  else if (ch == 'r')
+  {
+    generate();
+  }
   else if (ch == 'q' || ch == 'w' || ch == 'e' || ch == 'a' || ch == 'd' || ch == 'z' || ch == 's' || ch == 'c' || ch == 'W' || ch == 'S') 
   {
     movePlayer(player, ch);
@@ -311,20 +316,20 @@ void getInput(Entity *player)
   {
     switch (ch) 
     {
-    case KEY_UP:
-      if (selectedBlock < sizeof(blocks) - 1) selectedBlock++;
-      break;
-    case KEY_DOWN:
-      if (selectedBlock > 0) selectedBlock--;
-      break;
-    case KEY_LEFT:
-      mineBlock(player);
-      break;
-    case KEY_RIGHT:
-      placeBlock(player);
-      break;
-    default:
-      break;
+      case KEY_UP:
+        if (selectedBlock < sizeof(blocks) - 1) selectedBlock++;
+        break;
+      case KEY_DOWN:
+        if (selectedBlock > 0) selectedBlock--;
+        break;
+      case KEY_LEFT:
+        mineBlock(player);
+        break;
+      case KEY_RIGHT:
+        placeBlock(player);
+        break;
+      default:
+        break;
     }
   }
 }
@@ -332,8 +337,8 @@ void getInput(Entity *player)
 int main(int argc, char *argv[]) 
 {
   Entity player;
-  player.x = WIDTH / 2;
-  player.y = LENGTH / 2;
+  player.x = gameWIDTH / 2;
+  player.y = gameLENGTH / 2;
   player.z = 0;
   player.icon = '@';
 
@@ -358,17 +363,21 @@ int main(int argc, char *argv[])
   init_pair(3, COLOR_WHITE, COLOR_BLACK);  // stone
   init_pair(4, COLOR_BLACK, COLOR_BLACK);  // bedrock
 
-  WINDOW *game = newwin(LENGTH, WIDTH, 1, 1);
+  WINDOW *game = newwin(gameLENGTH, gameWIDTH, 1, 1);
+  WINDOW *inventory = newwin(inventoryLENGTH, inventoryWIDTH, gameLENGTH + 2, 1);
   box(game, 0, 0);
+  box(inventory, 0, 0);
   wrefresh(game);
+  wrefresh(inventory);
 
   generate();
 
   while (running) 
   {
     getInput(&player);
-    render(game, &player);
+    render(game, inventory, &player);
     wrefresh(game);
+    wrefresh(inventory);
   }
 
   endwin();
